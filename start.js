864df0ba0bf344138215f7bd33a1a1c71227707f
Dustin Lennon <92093+dustin-lennon@users.noreply.github.com>
2025-12-12 00:51:01 -0500

    feat: use start.js script to launch application with version display
    Create start.js wrapper script that displays version on startup and
    launches Next.js server. Update PM2 config to use start.js instead
    of directly calling bunx next start.
    
    Benefits:
    - Version is displayed when application starts
    - Centralized startup logic
    - Easier to add startup logging or checks in the future

---
+++ start.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { spawn } = require('child_process');
+
+// Get version from package.json
+const packageJson = require('./package.json');
+console.log(`Starting NightScout Backup Site v${packageJson.version}`);
+
+// Start the Next.js server with bunx
+const child = spawn('bunx', ['next', 'start'], {
+  stdio: 'inherit',
+  cwd: __dirname,
+  env: { ...process.env }
+});
+
+child.on('exit', (code) => {
+  process.exit(code);
+});
